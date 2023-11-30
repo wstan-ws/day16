aceb416633a401b3aba797d1bc7707d10e9d77ed
wstan-ws <wstan.ws97@gmail.com>
2023-11-30 11:17:37 +0800

    initial commit

---
+++ src/main/java/vttp/ssf/day16/Day16Application.java
@@ -0,0 +1,13 @@
+package vttp.ssf.day16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day16Application.class, args);
+	}
+
+}
